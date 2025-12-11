321181c7cddf85d028d683e8ff711c7ded59f16c
Wilson Bastidas <wilson.bastidas@pragma.com.co>
2025-12-11 17:50:15 -0500

    initial commit

---
+++ src/test/java/com/pragma/ms_traceability/MsTraceabilityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pragma.ms_traceability;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsTraceabilityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
